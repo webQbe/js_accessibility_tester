94f3f283bd780681b439ee216e3fc9d19f33ef7a
webQbe <webcubesolutionslk@gmail.com>
2024-11-28 19:47:33 +0530

    Run server & listen to PORT

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 5000; // Use 5000 if it is not in process.env
 
 // Intialize Express
 const app = express();
+
+// Run server & listen to port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
