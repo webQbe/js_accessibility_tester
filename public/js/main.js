20ee210a741fd830a89998ba729b1fa7511056f9
webQbe <webcubesolutionslk@gmail.com>
2024-11-28 20:49:05 +0530

    Added event listener to #form element

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+
+// Listen for submit event in the form
+document.querySelector('#form').addEventListener('submit', testAccessibility);
+
+
+// TODOS:
+    // Fetch Accessibility Issues
+    // Add Issues to DOM
+    // Set Loading State
+    // Escape HTML within text
+
+
+
