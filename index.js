84226175cdef9b99dbd2ba0417cbd794f619402c
webQbe <webcubesolutionslk@gmail.com>
2024-11-28 19:44:09 +0530

    Intialized express

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 const express = require('express');
 const pa11y = require('pa11y');
 const PORT = process.env.PORT || 5000; // Use 5000 if it is not in process.env
+
+// Intialize Express
+const app = express();
